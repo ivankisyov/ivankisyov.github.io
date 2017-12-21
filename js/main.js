49872d9d2116d6d9fd2acb730b22fbc93eaa645f
Vankata Kisyov <ivankisyov@gmx.com>
2017-12-21 06:49:59 +0200

    portfolio - i am ivan added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 jQuery(document).ready(function($) {
+
+	var $body = $('body');
+
 	$('#aboutMe, #contactMe').css('display', 'none');
 	$('a[data-rel^=lightcase]').lightcase({
 		showTitle: false
@@ -15,7 +18,6 @@ jQuery(document).ready(function($) {
      	$window = $(window),
      	$document = $(document);
 
-
     function showMainSmallScreens() {
 
     	if($window.outerWidth() < 600) {
@@ -29,5 +31,110 @@ jQuery(document).ready(function($) {
     showMainSmallScreens();
 
      $(window).on('scroll', showMainSmallScreens);
+
+     if($body.hasClass('i-am-ivan')) {
+
+        var $whoIam = $('#whoIam'),
+	     	$whoIamTarget = $('#whoIamTarget'),
+	     	$iAm = $('#iAm'),
+	     	$mainHeading = $('#mainHeading'),
+	     	$pageWrap = $('#pageWrap');
+
+     	var goodQualities = [
+     		'normal',
+     		'happy', 
+     		'good', 
+     		'caring', 
+     		'kind', 
+     		'bold', 
+     		'generous', 
+     		'calm', 
+     		'clever', 
+     		'handsome', 
+     		'the best', 
+     		'a lover', 
+     		'me', 
+     		'all'
+     	];
+
+     	var badQualities = [
+     		'crazy',
+     		'sad', 
+     		'bad', 
+     		'selfish',
+     		'heartless',
+     		'shy', 
+     		'greedy', 
+     		'angry', 
+     		'stupid', 
+     		'ugly', 
+     		'the worst', 
+     		'a hater', 
+     		'you', 
+     		'nothing'
+     	];
+
+        var allQualities = [];
+        var arrLength = goodQualities.length;
+
+        for(var i = 0; i < arrLength; i++) {
+            allQualities.push(badQualities[i]);
+            allQualities.push(goodQualities[i]);
+        }
+
+	    var counter = 0;
+	    var shown = true;
+	    var time = 333;    
+
+	    function changeContent() {
+	        if(counter === allQualities.length) {
+	            setTimeout(function() {
+	                $whoIamTarget.text('Ivan Kisyov');
+	                $whoIamTarget.removeClass('hidden');
+	            }, 777);
+	            return;
+	        } 
+	        $whoIamTarget.text(allQualities[counter]);
+	        if(shown) {
+	            setTimeout(function() {
+	                shown = false;
+	                    hideIt();
+	            }, 1000);
+	        } else {
+	            setTimeout(showIt, time);
+	        }
+	    }
+
+	    function hideIt() {
+	        counter++;
+	        $whoIamTarget.addClass('hidden');
+	        setTimeout(changeContent, time);
+	    }
+
+
+	    function showIt() {
+	        $whoIamTarget.removeClass('hidden');
+	        if(allQualities[counter] === 'you') {
+	        	time = 666;
+	        }
+	        if(allQualities[counter] === 'nothing') {
+	        	time = 999;
+	            $whoIam.addClass('bigger');
+	            $iAm.addClass('moveUp');
+	            $mainHeading.addClass('almost-there');
+	        }
+	        if(allQualities[counter] === 'all') {
+	            $whoIam.addClass('biggest');
+	            $iAm.addClass('moveUpMore');
+	            $mainHeading.addClass('finally-here');
+	            $pageWrap.addClass('yin-yang');
+	        }
+	        shown = true;
+	        changeContent();
+	    }
+
+	    changeContent()
+
+     }//if
 
 });
